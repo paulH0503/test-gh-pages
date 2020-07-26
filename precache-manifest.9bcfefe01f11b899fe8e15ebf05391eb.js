8e6ee6076c2aa3591db6663d879990c8781e68db
nguyen duc luong phat <nguyenducluongphat7049@gmail.com>
2020-07-26 15:18:49 +0700

    Updates

---
+++ precache-manifest.9bcfefe01f11b899fe8e15ebf05391eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed74f15501797c3c44bb785eb93d624",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "d2bd88bbf8443ab1e65b",
+    "url": "/test-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ba41520dbfd1f380670",
+    "url": "/test-gh-pages/static/js/2.2d48a55d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages/static/js/2.2d48a55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bd88bbf8443ab1e65b",
+    "url": "/test-gh-pages/static/js/main.659e82c3.chunk.js"
+  },
+  {
+    "revision": "6425981e8fa4c3102234",
+    "url": "/test-gh-pages/static/js/runtime-main.1c9f8b23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
